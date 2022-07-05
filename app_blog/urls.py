e21020acc3500418a0a732acdd73b482680c5f48
RezenkovD <rezenkovdmitro@gmail.com>
2022-07-05 13:07:48 +0300

    Save))))

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='index'), 
+   path('blog/', views.index, name='index'),
+   path('blog/blogs/', views.BlogListWiew.as_view(), name='blogs'),
+   path('blog/blog_authors/', views.AuthorListWiew.as_view(), name='blog_authors'),
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import blog_author, blog, blog_comment
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class BlogListWiew(generic.ListView):
+    model = blog
+
+class AuthorListWiew(generic.ListView):
+    model = blog_author
