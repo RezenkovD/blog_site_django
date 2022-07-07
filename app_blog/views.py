15fe61b01d6d4b0545ef400988fd91afcac031c7
RezenkovD <rezenkovdmitro@gmail.com>
2022-07-07 13:46:37 +0300

    Permission

--- app_blog/views.py
+++ app_blog/views.py
@@ -27,33 +27,39 @@ class BlogAuthorDetailView(generic.DetailView):
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-class blogcreate(LoginRequiredMixin, CreateView):
+class blogcreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'app_blog.add_blog'    
     model = blog
     fields = '__all__'
 
-class blogdelete(LoginRequiredMixin, DeleteView):
+class blogdelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'app_blog.delete_blog'
     model = blog
     success_url = reverse_lazy('blogs')
 
-class blogupdate(LoginRequiredMixin, UpdateView):
+class blogupdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'app_blog.change_blog'
     model = blog
     fields = '__all__'
     
-class authorcreate(LoginRequiredMixin, CreateView):
+class authorcreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'app_blog.add_blog_author'
     model = blog_author
     fields = '__all__'
 
-class authordelete(LoginRequiredMixin, DeleteView):
+class authordelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'app_blog.delete_blog_author'
     model = blog_author
     success_url = reverse_lazy('authors')
 
-class authorupdate(LoginRequiredMixin, UpdateView):
+class authorupdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'app_blog.change_blog_author'
     model = blog_author
     fields = '__all__'
 
 from django.urls import reverse
-
 
 class BlogCommentCreate(LoginRequiredMixin, CreateView):
     model = blog_comment
